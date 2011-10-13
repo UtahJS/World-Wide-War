848cd7d4d5e9a1c7a7625e66277a2bd46e39df0f
Dan Baker <dan.baker@corda.com>
2011-10-13 08:53:30 -0600

    Made map an object (not a singleton). Started game object

--- game.js
+++ game.js
@@ -1,13 +1,26 @@
+// Game Handler
+// A "Game" consists of multiple people and 1 map
+//
+// USAGE:
+// var gameConstuctor = require('game');
+// var myGame = new gameConstructor.newGame();
+
+
+var mapConstructor = require('./lib/map')
+  ;
 
 
 
 var newGame = (function() {
 	var name = 'private';
+	var map;
 	
 	// constructor
-	return function(name){
+	return function(width) {
+		map = new mapConstructor.newMap(width);		// create a new map that is 1000 pixels wide
 		this.name = name;
-	}
+	};
+
 })();
 
 
